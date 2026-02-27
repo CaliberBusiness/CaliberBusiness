c1608c8d32d1d48a0f5fbcf42c0f01a4bc3c3840
Xed8 <139227791+Xed8@users.noreply.github.com>
2026-02-28 00:55:03 +0800

    fix: remove distDir for vercel compatibility

--- next.config.js
+++ next.config.js
@@ -3,7 +3,6 @@ const isProd = process.env.NODE_ENV === 'production';
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   trailingSlash: true,
-  distDir: 'dist',
   images: {
     unoptimized: true
   },
